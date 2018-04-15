12a93fd39fe700654183a979491162e83d6b4904
Sibtain Raza Jamali <sibtainraza158@gmail.com>
2018-04-15 01:07:26 +0000

    Update script.py

--- script.py
+++ script.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     fnames_urls = parse_dataset(dataset, outdir)
 
     # download data
-    pool = multiprocessing.Pool(processes=500)
+    pool = multiprocessing.Pool(processes=50)
     with tqdm(total=len(fnames_urls)) as progress_bar:
         for _ in pool.imap_unordered(download_image, fnames_urls):
             progress_bar.update(1)
